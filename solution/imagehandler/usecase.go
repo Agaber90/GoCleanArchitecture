c06b3a5c27bae24c3138126ee210a62f2b79009c
Agaber90 <50577177+Agaber90@users.noreply.github.com>
2019-11-06 00:13:32 +0200

    Update usecase.go

--- solution/imagehandler/usecase.go
+++ solution/imagehandler/usecase.go
@@ -7,7 +7,6 @@ import (
 
 //Usecase represent the image's handler usecases
 type UseCase interface {
-	// AddImage(parCtx context.Context, parImg *models.DetectifyImage) error
 	AddImage(parCtx context.Context, parURLReq *models.SitesRequest) error
 	UpdateImage(parCtx context.Context, parImg *models.DetectifyImage) error
 	GetImageByID(parCtx context.Context, parID int64) (*models.DetectifyImage, error)
